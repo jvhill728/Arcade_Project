b7370814600abab4b040f8a63246a844c046d6dc
Johnny Hill <jvhill728@gmail.com>
2022-01-29 09:05:34 -0600

    creating repo for git hub

---
+++ script.js
@@ -0,0 +1,28 @@
+  /* Building a tic tac toe game where players x,o will be able to interact with the
+  board. Game will end with x or o winning or a tie. 
+  */
+
+// gameState is the constant state of the board, a grid set with null variables that will have 
+// Xs or Os to play through the game.  
+const gameState = {
+    players: ['X', 'O'],
+    board: [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ]
+}
+
+// Tic tac toe has 8 winning conditions. 3 vertical, 3 horizontal, 2 diagonal. Winning 
+// conditions will have to be declared using the array index of the grid. 
+
+const winningGame = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
