32a9346d74ba8462efecc7b89fdb8f21db2375a8
Johnny Hill <jvhill728@gmail.com>
2022-02-05 11:29:07 -0600

    improvements to display functionality

--- script.js
+++ script.js
@@ -4,55 +4,43 @@ to be used later to display messages on screen depending on game outcome
 */
 let gameActive = true;
 let currentPlayer = "X";
-let board = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-];
+let gameState = ["", "", "", "", "", "", "", "", ""];
+
 
 const playerTurnDisplay = document.querySelector(".playerTurn-display");
 const cells = Array.from(document.querySelectorAll(".cell"));
 const restartBtn = document.getElementById("restart");
-
+const gameStatus = document.querySelector(".game-outcome");
 
 // Write a function that will store user input and display it to the screen
 
-const player1Submit = document.getElementById("playerBtn1");
-player1Submit.addEventListener("click", function() {
-    let player1Input = document.getElementById("player1").value;
-    document.getElementById("player1-display").innerHTML = player1Input + " is Xs!";
-});
+    const player1Submit = document.getElementById("playerBtn1");
+    player1Submit.addEventListener("click", function() {
+        let player1Input = document.getElementById("player1").value;
+        document.getElementById("player1-display").innerHTML = player1Input + " is Xs!";
+        document.getElementById("player1").value = "";
+    });
 
-const player2Submit = document.getElementById("playerBtn2");
-player2Submit.addEventListener("click", function() {
-    let player2Input = document.getElementById("player2").value;
-    document.getElementById("player2-display").innerHTML = player2Input + " is Os!";
-});
+    const player2Submit = document.getElementById("playerBtn2");
+    player2Submit.addEventListener("click", function() {
+        let player2Input = document.getElementById("player2").value;
+        document.getElementById("player2-display").innerHTML = player2Input + " is Os!";
+        document.getElementById("player2").value = "";
+    });
 
 
-
-const gameWon = function() {
-    `Congratulations, ${currentPlayer} ! You Won!`;
-}    
-const gameDraw = function() {
-     "Cat's Game!";
-}
-
-const currPlayerTurn = function () {
-    `${currentPlayer} Go!`;
-}
 
 
 // gameState is the constant state of the board, a grid set with null variables that will have 
 // Xs or Os to play through the game.  
-const gameState = {
-    players: ['X', 'O'],
-    board: [
-        [null, null, null],
-        [null, null, null],
-        [null, null, null]
-    ]
-}
+// const gameState = {
+//     players: ['X', 'O'],
+//     board: [
+//         [null, null, null],
+//         [null, null, null],
+//         [null, null, null]
+//     ]
+// }
 
 // Tic tac toe has 8 winning conditions. 3 vertical, 3 horizontal, 2 diagonal. Winning 
 // conditions will have to be declared using the array index of the grid. 
@@ -70,22 +58,24 @@ const winningGame = [
 
 
 
+// Adding function to allow cells to be be clicked and display x or o depending on player turn
+function cellPlayed(clickedCell, clickedCellIndex) {
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
 
-// function handleCellPlayed(clickedCell, clickedCellIndex) {
-//     gameState[clickedCellIndex] = currentPlayer;
-//     clickedCell.innerHTML = currentPlayer;
-// }
 
-// function handlePlayerChange() {
-//     currentPlayer = currentPlayer === "X" ? "O" : "X";
-//     playerTurnDisplay.innerHTML = currentPlayerTurn();
-// }
+// Function will allow player turn to change by looking at who current player is and flipping it
+function playerChange() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    playerTurnDisplay.innerHTML = `${currentPlayer} Your Turn!`;
+}
 
-// Function to look at the board and analyze if it is a win or not
 
-function handleResultValidation() {
+// Function to look at cells played and determine winner, draw, if moves are valid
+function resultValidation() {
     let roundWon = false;
-    for (let i = 0; i <= 9; i++) {
+    for (let i = 0; i <= 7; i++) {
         const winCondition = winningGame[i];
         let a = gameState[winCondition[0]];
         let b = gameState[winCondition[1]];
@@ -95,88 +85,55 @@ function handleResultValidation() {
         }
         if (a === b && b === c) {
             roundWon = true;
-            break;
+            break
         }
     }
 
     if (roundWon) {
-        playerTurnDisplay.innerHTML = gameWon();
+        gameStatus.innerHTML = `The ${currentPlayer}'s Are Victorious!`;
         gameActive = false;
         return;
     }
 
     let roundDraw = !gameState.includes("");
     if (roundDraw) {
-        playerTurnDisplay.innerHTML = gameDraw();
+        gameStatus.innerHTML = "Cat's Game!";
         gameActive = false;
         return;
     }
 
+    playerChange();
 }
 
-// Function to check cells clicked by X or O so the same cell cannot be played twice.
-const isValidAction = (cell) => {
-    if (cell.innerText === 'X' || cell.innerText === 'O'){
-        return false;
+
+function cellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute("data-cell-index"));
+
+    if (gameState[clickedCellIndex] !== "" || !gameActive) {
+        return;
     }
 
-    return true;
-};
-
-// function to update the board with current player info
-const boardUpdate =  (index) => {
-    board[index] = currentPlayer;
-}
-
-// Funtion to change status display to alert which player's turn it is 
-const changePlayer = function() {
-    playerTurnDisplay.innerHTML = `It's ${currentPlayer}'s turn`;
-    currentPlayer = currentPlayer === "Xs Go!" ? "Os Gos!" : "Xs Go!";
-    playerTurnDisplay.innerHTML = currentPlayer;
-    playerTurnDisplay.classList.add(`{currentPlayer}`);
+    cellPlayed(clickedCell, clickedCellIndex);
+    resultValidation();
 }
 
 
-// Function is to definte user action and use helper functions to validate, update, and change player
-const userAction = function(cell, index) {
-    if(isValidAction(cell) && gameActive) {
-        cell.innerHTML = currentPlayer;
-        cell.classList.add(`Player${currentPlayer}`);
-        boardUpdate(index);
-        handleResultValidation();
-        changePlayer();
-    }
-}
-
-
-const resetBoard = () => {
-    board = [
-        [null, null, null],
-        [null, null, null],
-        [null, null, null]
-    ];
+// Restart function to clear board, player names, game status info
+const restartBoard = () => {
+    gameActive = true;
+  
     document.getElementById("player1-display").innerHTML = "";
     document.getElementById("player2-display").innerHTML = "";
     document.getElementById("player1").value = null;
     document.getElementById("player2").value = null;
+    document.getElementById("playerTurn").innerHTML = "";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
 
-    isGameActive = true;
-    // announcer.classList.add('hide');
-    
+}    
 
-    if (currentPlayer === 'O') {
-        changePlayer();
-    }
 
-    cells.forEach(cell => {
-        cell.innerText = '';
-        cell.classList.remove('playerX');
-        cell.classList.remove('playerO');
-    });
-}
 
-cells.forEach( (cell, index) => {
-    cell.addEventListener('click', () => userAction(cell, index));
-});
-
-restartBtn.addEventListener('click', resetBoard);
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener("click", cellClick));
+restartBtn.addEventListener("click", restartBoard);
